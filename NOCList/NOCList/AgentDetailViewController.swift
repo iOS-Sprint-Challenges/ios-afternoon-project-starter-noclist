f4f768a5c9e27edadb9ee6fdf9e31204f41198b5
Ben Gohlke <jcgohlke@me.com>
2019-08-05 11:23:21 -0400

    Updated starter project to fall in line with guided project.

---
+++ NOCList/NOCList/AgentDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AgentDetailViewController.swift
+//  NOCList
+//
+//  Created by Ben Gohlke on 8/5/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AgentDetailViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var coverNameLabel: UILabel!
+    @IBOutlet weak var realNameLabel: UILabel!
+    @IBOutlet weak var accessLevelLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- NOCList/NOCList/NOCListTableViewController.swift
+++ NOCList/NOCList/NOCListTableViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class NOCListTableViewController: UITableViewController
 {
-    // MARK: - Properties
-    
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
