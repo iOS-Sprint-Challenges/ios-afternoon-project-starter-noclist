9fb4f64973b57f7094745ffafb159e859f4c9ffc
fritzgt <tuanis@live.com>
2020-07-08 17:22:39 +0300

    Passed model unit test

---
+++ NOCList/NOCList/Model/Agent.swift
@@ -0,0 +1,17 @@
+//
+//  Agent.swift
+//  NOCList
+//
+//  Created by FGT MAC on 7/8/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct Agent {
+    var coverName: String
+    var realName: String
+    var accessLevel: Int
+    var compromised: Bool
+}
--- NOCList/NOCListTests/NOCListTests.swift
+++ NOCList/NOCListTests/NOCListTests.swift
@@ -7,27 +7,20 @@
 //
 
 import XCTest
+@testable import NOCList//Import because the target project being test resides in a different directory
 
 class NOCListTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func testAgentModel() {
+        let agent = Agent(coverName: "James Bond", realName: "Fritz", accessLevel: 5, compromised: false)
+        
+        XCTAssertNotEqual(agent.coverName, "992020")
+        XCTAssertEqual(agent.realName, "Fritz")
+        XCTAssertLessThan(agent.accessLevel, 10)
+        XCTAssertFalse(agent.compromised)
     }
+    
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 
 }
