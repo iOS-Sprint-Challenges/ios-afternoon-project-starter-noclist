a1df983dcb981f0e664da12243e53b1f6f256ba2
fritzgt <tuanis@live.com>
2020-07-09 11:44:01 +0300

    completed ui testing

--- NOCList/NOCListUITests/NOCListUITests.swift
+++ NOCList/NOCListUITests/NOCListUITests.swift
@@ -19,10 +19,27 @@ class NOCListUITests: XCTestCase {
         continueAfterFailure = false
         app.activate()
     }
-
-    func testCell() {
-        app.tables/*@START_MENU_TOKEN@*/.staticTexts["Ethan Hunt"]/*[[".cells.matching(identifier: \"AgentReusableCell\").staticTexts[\"Ethan Hunt\"]",".staticTexts[\"Ethan Hunt\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+    
+    func testDetailVC() {
         
+        app.tables.staticTexts["Ethan Hunt"].tap()
+        
+        let coverTextField = app.staticTexts["CoverTextField"]
+        let realTextField = app.staticTexts["RealNameTextField"]
+        let accessLevel = app.staticTexts["AccessLevel"]
+        
+        //Test values
+        XCTAssertNotNil(coverTextField.value)
+        XCTAssertNotNil(realTextField.value)
+        XCTAssertNotNil(accessLevel.value)
+        
+        //Go back to tableView
         app.navigationBars["Agent Details"].buttons["NOC List"].tap()
+    }
+    
+    
+    func testExposedAgents() {
+        let compromisedAgentsCount = XCUIApplication().tables.staticTexts["5 agents compromised"]
+        XCTAssertTrue(compromisedAgentsCount.exists)
     }
 }
