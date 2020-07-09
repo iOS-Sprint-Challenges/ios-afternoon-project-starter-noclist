b611c5d0e2d07a5da81bca93e172e2d7c7973417
fritzgt <tuanis@live.com>
2020-07-09 09:59:23 +0300

    cell unit test completed

--- NOCList/NOCListUITests/NOCListUITests.swift
+++ NOCList/NOCListUITests/NOCListUITests.swift
@@ -9,35 +9,20 @@
 import XCTest
 
 class NOCListUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    
+    private var app: XCUIApplication {
+        return XCUIApplication()
+    }
+    
+    //Overwrite original method
+    override func setUp() {
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.activate()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testCell() {
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["Ethan Hunt"]/*[[".cells.matching(identifier: \"AgentReusableCell\").staticTexts[\"Ethan Hunt\"]",".staticTexts[\"Ethan Hunt\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        app.navigationBars["Agent Details"].buttons["NOC List"].tap()
     }
 }
